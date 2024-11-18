e91976ead2a1f643546aa4bdea2fdc3955167c03
Divinefred123 <divine.frederick-idehai@binghamuni.edu.ng>
2024-11-18 21:44:03 +0100

    Add files via upload

---
+++ cos_101.py
@@ -0,0 +1,37 @@
+# speed = d/t
+ #input this is for speed
+print('this is for calculating speed')
+d = float(input('input distance: '))
+t = float(input('input time: '))
+speed = d/t
+print('the speed is= ', speed, ' km/hr')
+
+#density = m/v
+print('this is for calculating density')
+m = float(input('input mass: '))
+v = float(input('input volume: '))
+density = m/v
+print('the density is= ', density, ' kg/m3')
+
+# power = w/t
+print('this is calculating power')
+w = float(input('input work: '))
+t = float(input('input time: '))
+power = w/t
+print('the power is= ', power, 'J/s')
+
+#acceleration = F/m
+# input this is for acceleration
+print('this is for calculating acceleration')
+F = float(input('input net force: '))
+m = float(input('input mass: '))
+acceleration = F/m
+print('the acceleration is= ', acceleration, 'm s-2')
+
+#force = M*A
+# input this for force
+print('this is for calculating force')
+M = float(input('input mass: '))
+A = float(input('input acceleration: '))
+force = F*A
+print('the force is= ',force, 'N')
